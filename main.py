a30d008f84e8b25a9a60a6d68c60da7c07cf8541
Lucas <lucas.aubriet@outlook.fr>
2025-09-26 10:20:43 -0400

    Ajout main.py

---
+++ main.py
@@ -0,0 +1,29 @@
+#######################################################
+# TP1 - TCP scanner
+# Author: Romane Lesueur, Lucas Aubriet
+#######################################################
+
+import socket
+import re
+
+#def scanner(target :str, start_port :int, end_port :int):
+
+
+pattern = r'^((25[0-4]|2[0-4]\d|1\d{2}|[1-9]\d|\d)\.){3}(25[0-4]|2[0-4]\d|1\d{2}|[1-9]\d|\d)$'
+
+print("Welcome to the TCP scanner :)")
+target = str(input("Enter the target IP address: "))
+
+if re.fullmatch(pattern, target):
+    print(f"'{target}' est une IP valide !")
+else:
+    print(f"'{target}' n'est pas une IP valide.")
+
+answer = str(input("Do you want scan port 0 to 1024 ? (y/n) :"))
+
+if (answer == "y"):
+    scanner(target, 0, 1024)
+elif answer == "n":
+    port_range = str(input("Enter the port range (e.g., 20-80) :"))
+    start_port, end_port = map(int, port_range.split('-'))
+    scanner(target, start_port, end_port)
